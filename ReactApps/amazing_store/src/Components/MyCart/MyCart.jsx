acbdb3c0830b5b8f205fd4764591386d59d99db9
Gabriel Solomon <gabriel.solomon@softvision.ro>
2019-07-31 17:28:24 +0300

    SideBar

--- ReactApps/amazing_store/src/Components/MyCart/MyCart.jsx
+++ ReactApps/amazing_store/src/Components/MyCart/MyCart.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "../MyCart/MyCart.css";
 const MyCart = () => {
   return (
-    <a href="/">
-      <div className="myCart">
-        <i className="fas fa-cart-plus cart-icon" />
-        <div className="myCart-text">My Cart</div>
-        <div className="count"> 12 </div>
-      </div>
-    </a>
+    <button
+      type="button"
+      onClick={() => console.log("ceva")}
+      className="myCart"
+    >
+      <i className="fas fa-cart-plus cart-icon" />
+      <div className="myCart-text">My Cart</div>
+      <div className="count"> 12 </div>
+    </button>
   );
 };
 export default MyCart;
