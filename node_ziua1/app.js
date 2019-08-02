af74c908a28b9253355d86084e78fcf61d427f52
Gabriel Solomon <gabriel.solomon@softvision.ro>
2019-08-02 17:43:36 +0300

    Adaugare de la tastatura produse

---
+++ node_ziua1/app.js
@@ -0,0 +1,13 @@
+const add = require("./add");
+// add.produs("asd", "dasdas", "dasd");
+// const produs1 = {
+//   poza: "Ceva",
+//   descriere: "Altceva",
+//   pret: "13"
+// };
+// let stringProd1 = JSON.stringify(produs1);
+// add.addProduct(stringProd1);
+add.citireConsole();
+// const add2 = new add("asdas", "dasd", "s");
+// console.log(add2.afisare);
+// console.log(add("sa", "dasd", "dasdasd"));
